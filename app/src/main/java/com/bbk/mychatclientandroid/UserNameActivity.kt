e80590916f8bc0d90a538b8f4ba9aee948f6a000
EihabAhmed <eihabahmed@gmail.com>
2024-03-08 14:40:25 +0200

    Initial Commit

---
+++ app/src/main/java/com/bbk/mychatclientandroid/UserNameActivity.kt
@@ -0,0 +1,11 @@
+package com.bbk.mychatclientandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_name)
+    }
+}
