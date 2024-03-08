5d5fbf5d036b4ccb4336a09f3ee49596400d2904
EihabAhmed <eihabahmed@gmail.com>
2024-03-08 17:24:50 +0200

    Added chat

--- app/src/main/java/com/bbk/mychatclientandroid/UserNameActivity.kt
+++ app/src/main/java/com/bbk/mychatclientandroid/UserNameActivity.kt
@@ -1,11 +1,36 @@
 package com.bbk.mychatclientandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import com.bbk.mychatclientandroid.databinding.ActivityUserNameBinding
 
 class UserNameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserNameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_name)
+        binding = ActivityUserNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.etUsername.doAfterTextChanged {
+            val username = it.toString()
+            binding.btnProceed.isEnabled = username.isNotEmpty()
+        }
+        binding.btnProceed.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            if (username.isNotEmpty()) {
+                val intent = Intent(this, ChatActivity::class.java)
+                intent.putExtra(ChatActivity.USERNAME, username)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.etUsername.requestFocus()
     }
 }
